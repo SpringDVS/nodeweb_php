1b698eb8d22ae403a35f3630d78afa1bba46c01f
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-04-19 13:22:48 +0100

    testing: Add formatted string checks

--- testing/DvspNodeTest.php
+++ testing/DvspNodeTest.php
@@ -24,4 +24,28 @@ class DvspNodeTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals( Array(127,0,1,2), $node->address() );
 		$this->assertEquals( "node/", $node->resource() );
 	}
+	
+	public function testNodeStringFormat() {
+		$node = SpringDvs\Node::from_nodestring("spring,sub.host.tld,127.0.1.2");
+		
+		$this->assertEquals("spring,sub.host.tld,127.0.1.2", $node->toNodeString());
+	}
+	
+	public function testNodeStringFormatWithResource() {
+		$node = SpringDvs\Node::from_nodestring("spring,sub.host.tld/node/,127.0.1.2");
+		
+		$this->assertEquals("spring,sub.host.tld,127.0.1.2", $node->toNodeString());
+	}
+	
+	public function testNodeRegisterFormat() {
+		$node = SpringDvs\Node::from_nodestring("spring,sub.host.tld,127.0.1.2");
+		
+		$this->assertEquals("spring,sub.host.tld", $node->toNodeRegister());
+	}
+	
+	public function testNodeRegisterWithResource() {
+		$node = SpringDvs\Node::from_nodestring("spring,sub.host.tld/node/,127.0.1.2");
+		
+		$this->assertEquals("spring,sub.host.tld/node/", $node->toNodeRegister());
+	}
 }
