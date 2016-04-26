5885eb79de431bc0990a8c9f10954171f97f6612
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-04-26 17:48:51 +0100

    config: Change geosub for testing

--- system/config.php
+++ system/config.php
@@ -3,5 +3,5 @@
 \SpringDvs\Config::$spec['hostname'] = "nodedvs.greenman.zu";
 
 \SpringDvs\Config::$net['hostname'] = "spring.greenman.zu";
-\SpringDvs\Config::$net['geosub'] = "esusx";
+\SpringDvs\Config::$net['geosub'] = "localdom";
 \SpringDvs\Config::$spec['testing'] = true;
