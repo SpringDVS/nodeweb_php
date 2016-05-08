871f0323fbaf5cee21dc3ad9854537446038210f
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-05-08 11:12:20 +0100

    nwsbulletin: Start implementing api

--- system/modules/network/bulletin/config/controller.php
+++ system/modules/network/bulletin/config/controller.php
@@ -1,9 +1,31 @@
 <?php
 if(!defined('NODE_ADMIN')) die();
 
+use Flintstone\Flintstone;
+
 class ServiceController {
-	public static function request() {
-		//return json_encode()
+	private $db;
+	public function __construct() {
+		$options['dir'] = SpringDvs\Config::$sys['store_live'];
+		$this->db = new Flintstone('netservice_bulletin', $options);
+	}
+	public function request($action) {
+		$task = ($t = filter_input(INPUT_GET, 'task')) ?
+				$t : "none";
+		
+		
+		$method = $task.$action;
+		
+		
+		if(!method_exists('ServiceController', $method)) {
+			return json_encode([]);
+		}
+		
+		return $this->$method();
+	}
+	
+	private function newPost() {
+		return json_encode(['note' => 'ok']);
 	}
 }
 
