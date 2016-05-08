4689298bbe6cce1c650a5309fc01477a584606ef
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-05-08 23:16:36 +0100

    api: Add JSONP handler

--- system/controllers/GatewayApiController.php
+++ system/controllers/GatewayApiController.php
@@ -4,6 +4,10 @@ class GatewayApiController {
 	public function request($service) {
 		
 		$out = GatewayHandler::request($service);
+		$callback = filter_input(INPUT_GET, '__cb');
+		if($callback) {
+			$out = "$callback($out);";
+		}
 		return $out;
 	}
 }
