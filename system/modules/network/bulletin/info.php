f355d6edd945cb9f02350cabca4d293f55529695
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-05-09 13:44:46 +0100

    modules: Add encoding info
    This enables the service handler to encode response in json or not

--- system/modules/network/bulletin/info.php
+++ system/modules/network/bulletin/info.php
@@ -3,10 +3,11 @@
  * Author:  Charlie Fyvie-Gauld <cfg@zunautica.org>
  * License: Apache License, Version 2 (http://www.apache.org/licenses/LICENSE-2.0)
  */
-if(!defined('NODE_ADMIN')) return null;
+if(!defined('NODE_ADMIN') && !defined('SPRING_IF')) return null;
 
 return array(
 	'title' => 'Bulletin Service',
 	'description' => 'Post events, meetings and notices to the network',
 	'module' => 'bulletin',
+	'encoding' => 'json',
 );
--- system/modules/network/test/info.php
+++ system/modules/network/test/info.php
@@ -4,4 +4,5 @@ return array(
 	'title' => 'Test Service',
 	'description' => 'A service for testing',
 	'module' => 'test',
+	'encoding' => 'json',
 );
