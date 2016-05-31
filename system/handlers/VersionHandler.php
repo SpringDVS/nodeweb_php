33af4566266f50454882d92057a1121751d69dfd
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-05-31 10:28:39 +0100

    general: Add license header

--- system/handlers/VersionHandler.php
+++ system/handlers/VersionHandler.php
@@ -1,4 +1,8 @@
 <?php
+/* Notice:  Copyright 2016, The Care Connections Initiative c.i.c.
+ * Author:  Charlie Fyvie-Gauld <cfg@zunautica.org>
+ * License: Apache License, Version 2 (http://www.apache.org/licenses/LICENSE-2.0)
+ */
 
 class VersionHandler implements IVersionHandler {
 	private $src;
