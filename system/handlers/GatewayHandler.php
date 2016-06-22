e97e671bec9c5c8a144e3604ebb80a35e38e034c
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-06-22 14:49:50 +0100

    gateway: Add helper functions for gw services

--- system/handlers/GatewayHandler.php
+++ system/handlers/GatewayHandler.php
@@ -38,4 +38,65 @@ class GatewayHandler {
 				array_merge(['service' => $service],$response)
 			);
 	}
+	
+	/**
+	 * Perform a resolution of a Spring URI.
+	 * 
+	 * If there is an error then it returns false otherwise
+	 * it will return an array of objects that implement the
+	 * INodeNetInterface
+	 * 
+	 * @param string $uri
+	 * @return array(\SpringDvs\INodeNetInterface) | false 
+	 */
+	public static function resolveUri($uri) {
+		$message = SpringDvs\Message::fromStr("resolve $uri");
+		
+		$response = SpringDvs\HttpService::send($message, \SpringDvs\Config::$net['primary'],  \SpringDvs\Config::$net['hostname']);
+		
+		if(!$response
+		|| $response->cmd() != \SpringDvs\CmdType::Response
+		|| $response->content()->code() != \SpringDvs\ProtocolResponse::Ok) {
+			return false;
+		}
+		$type = $response->content()->type();
+		switch($type) {
+			case \SpringDvs\ContentResponse::Network:
+				return $response->content()->content()->nodes();
+			case \SpringDvs\ContentResponse::NodeInfo:
+				return array($response->content()->content());
+			default:
+				return false;
+		}
+		
+	}
+	
+	/**
+	 * Perform a request and accept first response
+	 * 
+	 *  This method takes an array of potential target nodes
+	 *  and if the request fails, it moves onto the next one.
+	 *  
+	 *  If there is no valid response then the entire method
+	 *  fails by returning null.
+	 * 
+	 * @param \SpringDvs\Message $msg
+	 * @param array $nodes
+	 * @return mixed \SpringDvs\Message on success | null on failure
+	 */
+	public static function outboundFirstResponse(\SpringDvs\Message $msg, array $nodes) {
+		
+		foreach($nodes as $node) {
+			$response = SpringDvs\HttpService::send($msg, $node->address(),  $node->host());
+
+			if($response === false
+			|| $response->cmd() != \SpringDvs\CmdType::Response
+			|| $response->content()->code() != \SpringDvs\ProtocolResponse::Ok) {
+				continue;
+			}
+			return $response;
+		}
+		
+		return null;
+	}
 }
